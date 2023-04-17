1ed050ce1d3adf0dad6bbebdd16c7b8d631f079e
Kis-ds <128333406+Kis-ds@users.noreply.github.com>
2023-04-18 08:56:45 +0900

    Update IB_1.py

--- IB_1.py
+++ IB_1.py
@@ -203,8 +203,8 @@ def get_report(info):
 st.title('공모회사채 발행시장 현황')
 
 # 날짜 선택
-start_date = st.date_input('시작일', value=date.today()- timedelta(days=1), max_value = date.today()- timedelta(days=1))
-max_date = min(start_date+timedelta(days=7), date.today()- timedelta(days=1))
+start_date = st.date_input('시작일', value=date.today(), max_value = date.today())
+max_date = min(start_date+timedelta(days=7), date.today())
 end_date = st.date_input('종료일', value=start_date, min_value = start_date, max_value = max_date)
 #st.write(datetime.strftime(aa,'%Y%m%d'))
 
