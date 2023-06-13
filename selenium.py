cb8ba01f65bc8a9a74307c72165d44dbc36d1f09
Kis-ds <128333406+Kis-ds@users.noreply.github.com>
2023-06-13 09:40:14 +0900

    Update selenium.py

--- selenium.py
+++ selenium.py
@@ -1,10 +1,11 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import warnings
-import streamlit as st
-
 warnings.filterwarnings('ignore')
 
 # 제어창(chromedriver) 실행
 driver = webdriver.Chrome('chromedriver.exe')
-# 네이버 이동
+# 네이버 화면 이동
 driver.get('https://www.naver.com')
+value = driver.find_element(By.XPATH,'/html/body/div[2]/div[2]/div/div[1]/div[1]/div[1]/div/ul/li[1]/span/a[1]').text
+value
