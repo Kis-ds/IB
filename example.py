aa908e18412e4ec48726429ba61158574d383186
Kis-ds <128333406+Kis-ds@users.noreply.github.com>
2023-06-13 12:59:57 +0900

    Update example.py

--- example.py
+++ example.py
@@ -1,23 +1,30 @@
 import streamlit as st
-from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
-# ChromeDriver 경로 설정
-chrome_driver_path = ChromeDriverManager().install()
-
-# ChromeDriver 서비스 생성
-service = Service(chrome_driver_path)
+import subprocess
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 # Chrome 옵션 설정
-chrome_options = webdriver.ChromeOptions()
+chrome_options = Options()
 chrome_options.add_argument("--headless")  # 브라우저 창을 열지 않고 실행하기 위해 headless 모드 설정
 
+# ChromeDriver 실행 명령
+command = "chromedriver"
+
 # ChromeDriver 실행
-with webdriver.Chrome(service=service, options=chrome_options) as driver:
-    # 웹 자동화 코드 작성
-    # ...
-    pass  # 필요한 동작 수행
+process = subprocess.Popen(command, stdout=subprocess.PIPE)
+
+# ChromeDriver가 실행될 때까지 잠시 대기
+process.stdout.readline()
+
+# ChromeDriver와 연결
+driver = webdriver.Chrome(options=chrome_options)
+
+# 웹 자동화 코드 작성
+# ...
 
 # ChromeDriver 종료
-service.stop()
+driver.quit()
+process.terminate()
