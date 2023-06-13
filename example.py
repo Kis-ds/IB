f5c7f0d67acc30d1bde6e8d235a717f7e147d9ca
Kis-ds <128333406+Kis-ds@users.noreply.github.com>
2023-06-13 12:03:30 +0900

    Update example.py

--- example.py
+++ example.py
@@ -5,12 +5,21 @@ from selenium.webdriver.common.by import By
 import time
 
 # ChromeDriverManager로 특정 버전의 ChromeDriver 설치
-driver = webdriver.Chrome(ChromeDriverManager().install())
+#driver = webdriver.Chrome(ChromeDriverManager().install())
 
 # 네이버 화면 이동
-driver.get('https://www.naver.com')
-time.sleep(5)
-value = driver.find_element(By.XPATH,'//*[@id="newsstand"]/div[2]/a').text
-st.write(value)
+#driver.get('https://www.naver.com')
+#time.sleep(5)
+#value = driver.find_element(By.XPATH,'//*[@id="newsstand"]/div[2]/a').text
+#st.write(value)
 # ChromeDriver 종료
-driver.quit()
+#driver.quit()
+try:
+    # ChromeDriver 설치
+    ChromeDriverManager().install()
+    
+    # ChromeDriver 버전 확인
+    version = webdriver.ChromeOptions().version['chrome']
+    st.write("Chrome 버전:", version)
+except Exception as e:
+    st.write("오류 발생:", str(e))
