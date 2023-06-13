4cd0fcec506a0b7c19d5ce8871bb3a4bb3b990bf
Kis-ds <128333406+Kis-ds@users.noreply.github.com>
2023-06-13 13:11:18 +0900

    Update example.py

--- example.py
+++ example.py
@@ -5,15 +5,15 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
-# ChromeDriver Manager를 사용하여 ChromeDriver 설치 및 경로 가져오기
-chrome_driver_path = ChromeDriverManager().install()
-
 # Chrome 옵션 설정
 chrome_options = Options()
 chrome_options.add_argument("--headless")  # 브라우저 창을 띄우지 않고 실행할 경우
 
-# ChromeDriver 실행
 try:
+    # ChromeDriver Manager를 사용하여 ChromeDriver 설치 및 경로 가져오기
+    chrome_driver_path = ChromeDriverManager().install()
+
+    # ChromeDriver 실행
     with webdriver.Chrome(service=Service(chrome_driver_path), options=chrome_options) as driver:
         # Streamlit 앱 구성
         st.title("Streamlit with Selenium")
