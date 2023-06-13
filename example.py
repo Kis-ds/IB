619f4864d30f78f32575e535a62dafc5de7dbff1
Kis-ds <128333406+Kis-ds@users.noreply.github.com>
2023-06-13 12:23:22 +0900

    Update example.py

--- example.py
+++ example.py
@@ -1,25 +1,28 @@
 import streamlit as st
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
-import time
-
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
+# Streamlit 앱 설정
+st.set_page_config(page_title="Selenium with Streamlit")
+
 # Chrome 드라이버 설치
-chrome_driver_path = ChromeDriverManager().install()
+chrome_options = Options()
+chrome_options.add_argument("--headless")  # 브라우저 창을 열지 않고 실행하려면 추가
+chrome_driver = ChromeDriverManager().install()
 
-# Chrome 브라우저 버전 가져오기
-options = Options()
-options.add_argument("--headless")  # 브라우저 창을 열지 않고 실행하려면 추가
-service = Service(chrome_driver_path)
-driver = webdriver.Chrome(service=service, options=options)
+# Streamlit 앱 내에서 Selenium 사용
+st.title("Selenium with Streamlit")
 
-# Chrome 버전 가져오기
-chrome_version = driver.capabilities["browserVersion"]
-st.write("Chrome 버전:", chrome_version)
+# 검색어 입력
+search_query = st.text_input("Enter a search query")
 
-# 드라이버 종료
-driver.quit()
+# 검색 버튼 클릭 시
+if st.button("Search"):
+    # Selenium으로 검색 결과 가져오기
+    with webdriver.Chrome(chrome_driver, options=chrome_options) as driver:
+        driver.get("https://www.google.com/search?q=" + search_query)
+        title = driver.title
+
+    # 검색 결과 타이틀 출력
+    st.write("Search Result Title:", title)
