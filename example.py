a1c320a97de5eed0955497b68924b4d6581e9a6c
Kis-ds <128333406+Kis-ds@users.noreply.github.com>
2023-06-13 10:32:10 +0900

    Update example.py

--- example.py
+++ example.py
@@ -1,11 +1,12 @@
 import re
 import subprocess
 import streamlit as st
-# 레지스트리를 통해 크롬 설치 경로 확인
-command = 'reg query "HKEY_CURRENT_USER\\Software\\Google\\Chrome\\BLBeacon" /v version'
+
+# 실행 가능한 명령어를 통해 크롬 버전 정보 얻기
+command = ['google-chrome', '--version']  # 또는 'chrome', 'chromium-browser' 등
 try:
-    output = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True).decode()
-    version = re.search(r"version\s*REG_SZ\s*(.*)", output).group(1)
-    st.write(f"Chrome 버전: {version}")
+    output = subprocess.check_output(command, stderr=subprocess.STDOUT).decode()
+    version = output.split(' ')[2].strip()
+    print(f"Chrome 버전: {version}")
 except subprocess.CalledProcessError as e:
-    st.write("Chrome 버전을 확인할 수 없습니다.")
+    print("Chrome 버전을 확인할 수 없습니다.")
