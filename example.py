3ad0c34900de9e0aad571472429ff3bb41186177
Kis-ds <128333406+Kis-ds@users.noreply.github.com>
2023-06-13 10:25:21 +0900

    Update example.py

--- example.py
+++ example.py
@@ -4,17 +4,14 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 import time
 
-def main():
-    # ChromeDriver 실행
-    driver = webdriver.Chrome(ChromeDriverManager().install())
+driver = webdriver.Chrome(ChromeDriverManager().install())
 
-    # 네이버 화면 이동
-    driver.get('https://www.naver.com')
-    time.sleep(5)
-    value = driver.find_element(By.XPATH,'//*[@id="newsstand"]/div[2]/a').text
-    st.write(value)
-    # ChromeDriver 종료
-    driver.quit()
+# 네이버 화면 이동
+driver.get('https://www.naver.com')
+time.sleep(5)
+value = driver.find_element(By.XPATH,'//*[@id="newsstand"]/div[2]/a').text
+st.write(value)
+# ChromeDriver 종료
+driver.quit()
 
-if __name__ == '__main__':
-    main()
+
