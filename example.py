4f7bc6a0a996e1feb062973d57072876acb255c1
Kis-ds <128333406+Kis-ds@users.noreply.github.com>
2023-06-13 13:27:38 +0900

    Update example.py

--- example.py
+++ example.py
@@ -1,34 +1,64 @@
-import streamlit as st
 import os
+
+import streamlit as st
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import By
 
-chrome_options = Options()
-chrome_options.add_argument("--headless")  # 브라우저 창을 띄우지 않고 실행할 경우
+options = Options()
+options.add_argument("--headless")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--disable-gpu")
+options.add_argument("--disable-features=NetworkService")
+options.add_argument("--window-size=1920x1080")
+options.add_argument("--disable-features=VizDisplayCompositor")
 
-try:
-    # ChromeDriver 설치 경로 확인
-    chrome_driver_path = ChromeDriverManager().install()
 
-    # ChromeDriver 실행 권한 부여
-    os.chmod(chrome_driver_path, 0o755)
+def delete_selenium_log():
+    if os.path.exists('selenium.log'):
+        os.remove('selenium.log')
 
-    # ChromeDriver 옵션 및 서비스 설정
-    driver_service = Service(chrome_driver_path)
-    driver_service.start()
 
-    with webdriver.Chrome(executable_path=chrome_driver_path, service=driver_service, options=chrome_options) as driver:
-        # Streamlit 앱 구성
-        st.title("Streamlit with Selenium")
+def show_selenium_log():
+    if os.path.exists('selenium.log'):
+        with open('selenium.log') as f:
+            content = f.read()
+            st.code(content)
 
-        # 크롤링 작업 등 수행
-        driver.get("https://www.example.com")
-        st.write(driver.title)
 
-    # ChromeDriver 서비스 종료
-    driver_service.stop()
+def run_selenium():
+    name = str()
+    with webdriver.Chrome(options=options, service_log_path='selenium.log') as driver:
+        url = "https://www.unibet.fr/sport/football/europa-league/europa-league-matchs"
+        driver.get(url)
+        xpath = '//*[@class="ui-mainview-block eventpath-wrapper"]'
+        # Wait for the element to be rendered:
+        element = WebDriverWait(driver, 10).until(lambda x: x.find_elements(by=By.XPATH, value=xpath))
+        name = element[0].get_property('attributes')[0]['name']
+    return name
 
-except Exception as e:
-    st.error(f"Failed to start ChromeDriver: {e}")
+
+if __name__ == "__main__":
+    delete_selenium_log()
+    st.set_page_config(page_title="Selenium Test", page_icon='✅',
+        initial_sidebar_state='collapsed')
+    st.title('🔨 Selenium Test for Streamlit Sharing')
+    st.markdown('''This app is only a very simple test for **Selenium** running on **Streamlit Sharing** runtime.<br>
+        The suggestion for this demo app came from a post on the Streamlit Community Forum.<br>
+        <https://discuss.streamlit.io/t/issue-with-selenium-on-a-streamlit-app/11563><br>
+        This is just a very very simple example and more a proof of concept.
+        A link is called and waited for the existence of a specific class and read it.
+        If there is no error message, the action was successful.
+        Afterwards the log file of chromium is read and displayed.
+        ---
+        ''', unsafe_allow_html=True)
+
+    st.balloons()
+    if st.button('Start Selenium run'):
+        st.info('Selenium is running, please wait...')
+        result = run_selenium()
+        st.info(f'Result -> {result}')
+        st.info('Successful finished. Selenium log file is shown below...')
+        show_selenium_log()
