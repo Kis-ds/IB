a0b5056db1a35968aad4a68b76aa0887eea38768
Kis-ds <128333406+Kis-ds@users.noreply.github.com>
2023-06-13 12:20:36 +0900

    Update example.py

--- example.py
+++ example.py
@@ -4,24 +4,18 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 import time
 
-# ChromeDriverManager로 특정 버전의 ChromeDriver 설치
-#driver = webdriver.Chrome(ChromeDriverManager().install())
-
-# 네이버 화면 이동
-#driver.get('https://www.naver.com')
-#time.sleep(5)
-#value = driver.find_element(By.XPATH,'//*[@id="newsstand"]/div[2]/a').text
-#st.write(value)
-# ChromeDriver 종료
-#driver.quit()
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 
 # Chrome 드라이버 설치
 chrome_driver_path = ChromeDriverManager().install()
 
 # Chrome 브라우저 버전 가져오기
-options = webdriver.ChromeOptions()
+options = Options()
 options.add_argument("--headless")  # 브라우저 창을 열지 않고 실행하려면 추가
-driver = webdriver.Chrome(chrome_driver_path, options=options)
+service = Service(chrome_driver_path)
+driver = webdriver.Chrome(service=service, options=options)
 
 # Chrome 버전 가져오기
 chrome_version = driver.capabilities["browserVersion"]
