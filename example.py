f08cc366e2a098efe5b7ea622331405020eaec08
Kis-ds <128333406+Kis-ds@users.noreply.github.com>
2023-06-13 13:09:17 +0900

    Update example.py

--- example.py
+++ example.py
@@ -1,26 +1,18 @@
 import streamlit as st
-import subprocess
-from bs4 import BeautifulSoup
-import requests
-
-# ChromeDriver 실행 파일 경로
-chromedriver_path = "/home/appuser/.wdm/drivers/chromedriver/linux64/114.0.5735.90/chromedriver"
-
-# 실행 권한 추가
-subprocess.run(["chmod", "+x", chromedriver_path], check=True)
+from selenium import webdriver
 
 # Streamlit 앱 시작
 st.title("Streamlit with Selenium")
 
-# 웹 크롤링 작업
-url = "https://www.example.com"  # 크롤링할 웹 페이지의 URL
-command = [chromedriver_path]
-process = subprocess.Popen(command, stdout=subprocess.PIPE)
-output = process.communicate()[0]
+# ChromeDriver 실행
+driver = webdriver.Chrome()
+driver.get("https://www.example.com")
 
-# 웹 페이지 파싱
-soup = BeautifulSoup(output, "html.parser")
-title = soup.title.string
+# 웹 페이지 제목 추출
+title = driver.title
 
 # 추출한 제목 출력
 st.write("Web Page Title:", title)
+
+# 드라이버 종료
+driver.quit()
