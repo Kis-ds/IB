21a8bd25e3b563c472fdc62bd4d85f3999f36207
Kis-ds <128333406+Kis-ds@users.noreply.github.com>
2023-06-13 10:34:26 +0900

    Update example.py

--- example.py
+++ example.py
@@ -1,12 +1,18 @@
 import re
 import subprocess
 import streamlit as st
+import winreg
 
-# 실행 가능한 명령어를 통해 크롬 버전 정보 얻기
-command = ['google-chrome', '--version']  # 또는 'chrome', 'chromium-browser' 등
-try:
-    output = subprocess.check_output(command, stderr=subprocess.STDOUT).decode()
-    version = output.split(' ')[2].strip()
-    st.write(f"Chrome 버전: {version}")
-except subprocess.CalledProcessError as e:
+def get_chrome_version():
+    try:
+        key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, r"Software\Google\Chrome\BLBeacon")
+        value, _ = winreg.QueryValueEx(key, "version")
+        return value
+    except FileNotFoundError:
+        return None
+
+chrome_version = get_chrome_version()
+if chrome_version:
+    st.write(f"Chrome 버전: {chrome_version}")
+else:
     st.write("Chrome 버전을 확인할 수 없습니다.")
