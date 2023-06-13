30236b4c42031726946ee90526246ac464c5169f
Kis-ds <128333406+Kis-ds@users.noreply.github.com>
2023-06-13 10:14:40 +0900

    Update example.py

--- example.py
+++ example.py
@@ -6,7 +6,7 @@ import time
 warnings.filterwarnings('ignore')
 
 # 제어창(chromedriver) 실행
-driver = webdriver.Chrome('chromedriver.exe')
+driver = webdriver.Chrome('./chromedriver.exe')
 # 네이버 화면 이동
 driver.get('https://www.naver.com')
 time.sleep(5)
