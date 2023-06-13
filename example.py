cfafd9c9aeee469f86b9849b0a4927f16fe24f4d
Kis-ds <128333406+Kis-ds@users.noreply.github.com>
2023-06-13 10:37:50 +0900

    Update example.py

--- example.py
+++ example.py
@@ -5,8 +5,7 @@ from selenium.webdriver.common.by import By
 import time
 
 # ChromeDriverManager로 특정 버전의 ChromeDriver 설치
-driver = webdriver.Chrome(ChromeDriverManager(version="114.0.5735.110").install())
-#driver = webdriver.Chrome(ChromeDriverManager().install())
+driver = webdriver.Chrome(ChromeDriverManager().install())
 
 # 네이버 화면 이동
 driver.get('https://www.naver.com')
