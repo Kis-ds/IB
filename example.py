055df00adef584b2acc4d2310304ecdc83649c23
Kis-ds <128333406+Kis-ds@users.noreply.github.com>
2023-06-13 10:06:54 +0900

    Update example.py

--- example.py
+++ example.py
@@ -1,11 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import warnings
+import streamlit as st
+import time
 warnings.filterwarnings('ignore')
 
 # 제어창(chromedriver) 실행
-driver = webdriver.Chrome('./chromedriver.exe')
+driver = webdriver.Chrome('chromedriver.exe')
 # 네이버 화면 이동
 driver.get('https://www.naver.com')
-value = driver.find_element(By.XPATH,'/html/body/div[2]/div[2]/div/div[1]/div[1]/div[1]/div/ul/li[1]/span/a[1]').text
-st.write(value)
+time.sleep(5)
