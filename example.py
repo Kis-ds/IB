a95c1df6142ebb88c188d2abcd88f8afafd66fbc
Kis-ds <128333406+Kis-ds@users.noreply.github.com>
2023-06-13 13:10:22 +0900

    Update example.py

--- example.py
+++ example.py
@@ -1,18 +1,26 @@
 import streamlit as st
+import sys
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 
-# Streamlit 앱 시작
-st.title("Streamlit with Selenium")
+# ChromeDriver Manager를 사용하여 ChromeDriver 설치 및 경로 가져오기
+chrome_driver_path = ChromeDriverManager().install()
+
+# Chrome 옵션 설정
+chrome_options = Options()
+chrome_options.add_argument("--headless")  # 브라우저 창을 띄우지 않고 실행할 경우
 
 # ChromeDriver 실행
-driver = webdriver.Chrome()
-driver.get("https://www.example.com")
+try:
+    with webdriver.Chrome(service=Service(chrome_driver_path), options=chrome_options) as driver:
+        # Streamlit 앱 구성
+        st.title("Streamlit with Selenium")
 
-# 웹 페이지 제목 추출
-title = driver.title
-
-# 추출한 제목 출력
-st.write("Web Page Title:", title)
-
-# 드라이버 종료
-driver.quit()
+        # 크롤링 작업 등 수행
+        driver.get("https://www.example.com")
+        st.write(driver.title)
+except Exception as e:
+    st.error(f"Failed to start ChromeDriver: {e}")
+    sys.exit(1)
